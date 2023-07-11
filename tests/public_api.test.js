712a383c6c00fa327e2ca043c1541296940d7c8c
Lucas Amaral <lopesdoamaral@gmail.com>
2023-07-10 22:55:06 -0300

    Add test to check count attribute

--- tests/public_api.test.js
+++ tests/public_api.test.js
@@ -10,4 +10,10 @@ describe("Public API lists", ()=>{
     test("should retrieve the list of all public APIs with success", async ()=> {
         expect(this.response.statusCode).toBe(200);
     });
+
+    test("should return entries count equals to the number of listed entries", async ()=> {
+        const count = this.response.body.count;
+        const entries = this.response.body.entries.length;
+        expect(count).toBe(entries);
+    });
 });
